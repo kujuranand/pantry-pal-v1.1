3b2552b234f2fdb46d64c8abc89f596b27afe0c0
Anand <anand.kujur@yahoo.com>
2025-09-21 16:15:07 +0930

    added models, updated gitignore - core

---
+++ PantryPal/PantryPal.Core/Models/GroceryList.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace PantryPal.Core.Models;
+
+[Table("GroceryLists")]
+public class GroceryList
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [NotNull]
+    public string Name { get; set; } = "";
+
+    [NotNull]
+    public DateTime CreatedUtc { get; set; }
+}
---
+++ PantryPal/PantryPal.Core/Models/GroceryListItem.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace PantryPal.Core.Models;
+
+[Table("GroceryListItems")]
+public class GroceryListItem
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    // FK to GroceryLists.Id
+    [Indexed, NotNull]
+    public int ListId { get; set; }
+
+    [NotNull]
+    public string Name { get; set; } = "";
+
+    [NotNull]
+    public decimal Cost { get; set; }
+
+    // Optional
+    public DateTime? PurchasedDate { get; set; }
+}
